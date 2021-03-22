4735a2306835287d7b3d52eb7f5033c62cc4d5d7
Kuldeep <recs.cse1625@gmail.com>
2021-03-22 18:52:06 +0530

    Updates

---
+++ precache-manifest.223f6f070cc61f8dc0dcddbe162665de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf172cc06c794827e4e047040ba121d7",
+    "url": "/eCommerceApp/index.html"
+  },
+  {
+    "revision": "42e5eb44d7ded625a09a",
+    "url": "/eCommerceApp/static/css/main.86414d2f.chunk.css"
+  },
+  {
+    "revision": "953ef33c369f5c750bba",
+    "url": "/eCommerceApp/static/js/2.f72fcec4.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/eCommerceApp/static/js/2.f72fcec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e5eb44d7ded625a09a",
+    "url": "/eCommerceApp/static/js/main.eea144df.chunk.js"
+  },
+  {
+    "revision": "3629e0a996f2b250746a",
+    "url": "/eCommerceApp/static/js/runtime-main.bedbd9d1.js"
+  }
+]);
